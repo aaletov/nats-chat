f2c7fbaf4d456f1c76d44e462ba1eb955619d219
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2023-08-01 19:13:09 +0300

    Manage containers via docker-py
    Signed-off-by: Artemy Pozdnyakov <aapozdnyakove@gmail.com>

--- Makefile
+++ Makefile
@@ -5,6 +5,4 @@ build:
 	go build -o ./build/nats-chat
 
 test: build
-	docker run --rm -d --name nats -p 4444:4444 nats:alpine3.18 -p 4444 -D --trace
-	python3 ./test/test-hello.py || docker stop nats 
-	docker stop nats
+	python3 ./test/test-hello.py
--- test/test-hello.py
+++ test/test-hello.py
@@ -5,58 +5,62 @@ import logging
 import sys
 import time
 import shutil
+import docker
 
 home = os.getenv("NATS_CHAT_HOME")
 
 class TestNatsChat(unittest.TestCase):
     def test_hello(self):
         logger = logging.getLogger("LOGGER")
+        client = docker.from_env()
+        container = client.containers.run("nats:alpine3.18", detach=True, auto_remove=True, 
+                                          ports={"4444/tcp": ("0.0.0.0", 4444)},
+                                          command="nats-server --config /etc/nats/nats-server.conf -p 4444")
+        while container.status != "running":
+            container.reload()
+            time.sleep(0.1)
 
-        args1 = (
+        args = (
             os.path.join(home, "build/nats-chat"),
             "run",
             "--profile",
-            os.path.join(home, "test/testprofile1"),
+            os.path.join(home, "test/{sender}"),
             "--recepient-key",
-            os.path.join(home, "test/testprofile2/public.pem"),
+            os.path.join(home, "test/{recepient}/public.pem"),
             "--nats-url",
             "nats://0.0.0.0:4444",
         )
 
-        args2 = (
-            os.path.join(home, "build/nats-chat"),
-            "run",
-            "--profile",
-            os.path.join(home, "test/testprofile2"),
-            "--recepient-key",
-            os.path.join(home, "test/testprofile1/public.pem"),
-            "--nats-url",
-            "nats://0.0.0.0:4444",
-        )
+        try:
+            args1 = tuple(arg.format(sender="testprofile1", recepient="testprofile2") for arg in args)
+            args2 = tuple(arg.format(sender="testprofile2", recepient="testprofile1") for arg in args)
 
-        p1 = subprocess.Popen(args1, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        p2 = subprocess.Popen(args2, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+            p1 = subprocess.Popen(args1, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+            p2 = subprocess.Popen(args2, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-        time.sleep(1)
+            time.sleep(1)
 
-        p1.stdin.write(bytes("Hello!\n", 'utf-8'))
-        p1.stdin.flush()
-        p2.stdin.write(bytes("Nice to meet you!\n", 'utf-8'))
-        p2.stdin.flush()
-        t1 = p1.stdout.readline().decode('utf-8')
-        t2 = p2.stdout.readline().decode('utf-8')
-        p1.kill()
-        p1.stdin.close()
-        p1.stdout.close()
-        p1.wait()
-        p2.kill()
-        p2.stdin.close()
-        p2.stdout.close()
-        p2.wait()
-        logger.info(t1)
-        logger.info(t2)
-        self.assertTrue("Nice to meet you!" in t1)    
-        self.assertTrue("Hello!" in t2)
+            p1.stdin.write(bytes("Hello!\n", 'utf-8'))
+            p1.stdin.flush()
+            p2.stdin.write(bytes("Nice to meet you!\n", 'utf-8'))
+            p2.stdin.flush()
+            t1 = p1.stdout.readline().decode('utf-8')
+            t2 = p2.stdout.readline().decode('utf-8')
+            p1.kill()
+            p1.stdin.close()
+            p1.stdout.close()
+            p1.wait()
+            p2.kill()
+            p2.stdin.close()
+            p2.stdout.close()
+            p2.wait()
+            logger.info(t1)
+            logger.info(t2)
+            self.assertTrue("Nice to meet you!" in t1)    
+            self.assertTrue("Hello!" in t2)
+        finally:
+            container.stop()
+            client.close()
 
     def test_generate(self):
         logger = logging.getLogger("LOGGER")
